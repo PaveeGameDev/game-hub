0856357b0a432cfc0b29a155f8ce78e2eef55303
Patrik Holba <Paveegamedev@gmail.com>
2023-09-26 18:15:54 +0200

    Create a custom hook for fetching the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+type Game = {
+  id: number;
+  name: string;
+};
+
+type FetchGamesResponse = {
+  count: number;
+  results: Game[];
+};
+
+export const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
