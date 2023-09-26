7c49d793da52be2a40692ae33fb73f9ef137031c
Patrik Holba <Paveegamedev@gmail.com>
2023-09-26 20:58:40 +0200

    Displaying CriticScore.tsx

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@chakra-ui/react";
+
+type Props = {
+  score: number;
+};
+
+export const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export type Game = {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 };
 
 type FetchGamesResponse = {
